5096cf6efee13c4d7d39c586586b2d176e34a5d9
valeshan <valeshan@gmail.com>
2017-12-20 00:16:56 +1100

    added capitalisation
    -added capitalisation to the name and city of employee, added into ajax request

--- js/main.js
+++ js/main.js
@@ -6,11 +6,14 @@ $(function(){
   	success: function(data){
        console.log(data.results);
       for(let i= 0; i <= 12; i++){
-            $empList.append('<li>'+  '<img src="' + data.results[i].picture.large +'"><div class = "info"'
-              + '<p class = "name"><b>' + data.results[i].name.first +' ' + data.results[i].name.last + '</b></p>'
+            $empList.append('<li>'+  '<a><img href = "#" src="' + data.results[i].picture.medium +'"><div class = "info"></a><a>'
+              + '<p class = "name"><b>' + data.results[i].name.first.charAt(0).toUpperCase()+data.results[i].name.first.slice(1)
+              +' ' + data.results[i].name.last.charAt(0).toUpperCase()+data.results[i].name.last.slice(1) + '</b></p>'
               + '<p class = "email">' + data.results[i].email + '</p>'
-              + '<p class = "city">' + data.results[i].location.city + '</p></div>'+'</li>');
+              + '<p class = "city">' + data.results[i].location.city.charAt(0).toUpperCase()+ data.results[i].location.city.slice(1)
+              + '</p></a></div>'+'</li>');
           }
       }
-});
+    });
+    $("li").css("background-color", "blue");
 });

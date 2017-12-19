1e55e2c9443ca99090c4d9de11f0a42918501424
valeshan <valeshan@gmail.com>
2017-12-19 15:45:47 +1100

    updated css
    -updated css to locate img and text locations in list item
    -added class to title
    -updated js to get large image

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ $(function(){
   	dataType: 'json',
   	success: function(people){
         console.log(people.results[0]);
-        $empList.append('<li>'+  '<img src="' + people.results[0].picture.medium +'">'
+        $empList.append('<li>'+  '<img src="' + people.results[0].picture.large +'"><div class = "info"'
         + '<p class = "name"><b>' + people.results[0].name.first +' ' + people.results[0].name.last + '</b></p>'
         + '<p class = "email">' + people.results[0].email + '</p>'
-        + '<p class = "city">' + people.results[0].location.city + '</p>'+'</li>');
+        + '<p class = "city">' + people.results[0].location.city + '</p></div>'+'</li>');
       }
 });
 });

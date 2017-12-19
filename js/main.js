e5d2ebaf619dea471e80b377af021a993c1e60da
valeshan <valeshan@gmail.com>
2017-12-19 13:21:09 +1100

    fixed ajax request
    -updated ajax request so that data is outputted
    -updated html - added list
    -updated css - added font and bkgd color

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-$.ajax({
-	Url: 'https://randomuser.me/api/',
-	dataType: 'json',
-	Success: function(data){
-		Console.log(data);
-	}
+$(function(){
+
+  const $empList = $(".employee-list");
+  $.ajax("https://randomuser.me/api/", {
+  	Url: 'https://randomuser.me/api/',
+  	dataType: 'json',
+    type: 'GET',
+  	success: function(people){
+        console.log(people.results[0]);
+        $empList.append('<li>name: '+ people.results[0].name.first +' ' + people.results[0].name.last + '</li>');
+      }
+});
 });

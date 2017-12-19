eda0c73b1fe00238bb388355bda7a6ce4483749d
valeshan <valeshan@gmail.com>
2017-12-19 21:23:46 +1100

    -fixed ajax request
    -ajax request now returns 12 employees, they currently are inserted into list items in the correct card format
    -list structure still needs to be corrected to a 3 by 4 grid.
    -modal feature also needs to be added

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 $(function(){
-
   const $empList = $(".employee-list");
-  $.ajax("https://randomuser.me/api/", {
-  	Url: 'https://randomuser.me/api/',
+  $.ajax('https://randomuser.me/api/?results=12', {
+  	url: 'https://randomuser.me/api/?results=12',
   	dataType: 'json',
-  	success: function(people){
-        console.log(people.results[0]);
-        $empList.append('<li>'+  '<img src="' + people.results[0].picture.large +'"><div class = "info"'
-        + '<p class = "name"><b>' + people.results[0].name.first +' ' + people.results[0].name.last + '</b></p>'
-        + '<p class = "email">' + people.results[0].email + '</p>'
-        + '<p class = "city">' + people.results[0].location.city + '</p></div>'+'</li>');
+  	success: function(data){
+       console.log(data.results);
+      for(let i= 0; i <= 12; i++){
+            $empList.append('<li>'+  '<img src="' + data.results[i].picture.large +'"><div class = "info"'
+              + '<p class = "name"><b>' + data.results[i].name.first +' ' + data.results[i].name.last + '</b></p>'
+              + '<p class = "email">' + data.results[i].email + '</p>'
+              + '<p class = "city">' + data.results[i].location.city + '</p></div>'+'</li>');
+          }
       }
 });
 });

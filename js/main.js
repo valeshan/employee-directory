9a442a25a696b0082f78f307ac7c9b7b43ccd82a
valeshan <valeshan@gmail.com>
2017-12-20 13:36:21 +1100

    added ModalShow function
    -added modalShow function which makes ajax request on click
    -need to figure how to make ajax request show address, and pop up on modal

--- js/main.js
+++ js/main.js
@@ -5,8 +5,9 @@ $(function(){
   	dataType: 'json',
   	success: function(data){
        console.log(data.results);
+
       for(let i= 0; i <= 12; i++){
-            $empList.append('<li>'+  '<a><img href = "#" src="' + data.results[i].picture.medium +'"><div class = "info"></a><a>'
+            $empList.append('<li>'+  '<a onClick= "modalShow()" ><img src="' + data.results[i].picture.medium +'"><div class = "info"></a><a>'
               + '<p class = "name"><b>' + data.results[i].name.first.charAt(0).toUpperCase()+data.results[i].name.first.slice(1)
               +' ' + data.results[i].name.last.charAt(0).toUpperCase()+data.results[i].name.last.slice(1) + '</b></p>'
               + '<p class = "email">' + data.results[i].email + '</p>'
@@ -15,5 +16,14 @@ $(function(){
           }
       }
     });
-    $("li").css("background-color", "blue");
+
 });
+function modalShow(){
+  $.ajax('https://randomuser.me/api/', {
+  	url: 'https://randomuser.me/api/',
+  	dataType: 'json',
+  	success: function(data){
+       console.log($(this).data.results[0].location.street);
+      }
+    });
+}

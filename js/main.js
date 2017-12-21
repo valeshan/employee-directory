d53b0626df4c68c300eff1ecf619a3c25f654815
valeshan <valeshan@gmail.com>
2017-12-22 01:15:54 +1100

    -fixed formating
    -fixed formating of modal content, incl. tel number, state initials and capitalisation
    -added close button, completed
    -added next button, not yet functional
    -changed overlay opacity to 0.5

--- js/main.js
+++ js/main.js
@@ -4,11 +4,22 @@ const $empList = $(".employee-list");
 //append modal
 const $overlay = $("<div id='overlay'></div>");
 const $modal = $('<div id = "modal"></div>');
+
+//close button
+const $close = $('<button id = "close"> X </button>');
+
+//modal content
 const $image = $('<img id="modal-image">');
 const $basicInfo = $("<div id = 'basicInfo'></div>");
 const $secondaryInfo = $("<div id = 'secondaryInfo'></div>");
 
+//next and prev buttons
+const $prev = $('<button id = "prev"> < </button>');
+const $next = $('<button id = "next"> > </button>');
 
+$modal.append($prev);
+$modal.append($next);
+$modal.append($close);
 $modal.append($image);
 $modal.append($basicInfo);
 $modal.append($secondaryInfo);
@@ -20,10 +31,10 @@ $("body").append($overlay);
 function empList(employees){
   $.each(employees, function(i, employee){
         $empList.append('<li class= "employee" id = "'+ i + '">'+  '<a><img src="' + employee.picture.large +'"><div class = "info">'
-          + '<p class = "name primary"><b>' + employee.name.first.charAt(0).toUpperCase()+employee.name.first.slice(1)
-          +' ' + employee.name.last.charAt(0).toUpperCase()+employee.name.last.slice(1) + '</b></p>'
+          + '<p class = "name primary"><b>' + capitalise(employee.name.first)
+          +' ' + capitalise(employee.name.last) + '</b></p>'
           + '<p class = "email secondary">' + employee.email + '</p>'
-          + '<p class = "city secondary">' + employee.location.city.charAt(0).toUpperCase()+ employee.location.city.slice(1)
+          + '<p class = "city secondary">' + capitalise(employee.location.city)
           + '</p></a></div>'+'</li>');
         });
 }
@@ -49,12 +60,14 @@ function modalShow(employees){
 
     $basicInfo.append(`<p class = "name primary"><b>${name}<b></p>`);
     $basicInfo.append(`<p class = "email secondary"> ${email}</p>`);
-    $basicInfo.append(`<p class = "city secondary"> ${city}</p><hr>`);
+    $basicInfo.append(`<p class = "city secondary"> ${city}</p>`);
+
+    $basicInfo.append(`<hr>`);
 
     //secondary info of employee
-    const phone = employees[id].phone;
-    const street = employees[id].location.street;
-    const state = employees[id].location.state.charAt(0).toUpperCase()+employees[id].location.state.slice(1);;
+    const phone = pFormat(employees[id].phone);
+    const street = capitalise(employees[id].location.street);
+    const state = initLetters(employees[id].location.state);
     const postcode = employees[id].location.postcode;
     const day = employees[id].dob.slice(8,10);
     const month = employees[id].dob.slice(5,7);
@@ -65,11 +78,75 @@ function modalShow(employees){
     $secondaryInfo.append(`<p class = "dob tertiary">Birthday:  ${day}/${month}/${year}</p>`);
 
     $overlay.show();
+    $modal.show();
   });
 }
 
-$(function(){
+
+//capitalise
+function capitalise(entry){
+  let capEntry = "";
+  const strArray = entry.split(" ");
+  $.each(strArray, function(i, word){
+    capEntry += word.charAt(0).toUpperCase() + word.slice(1) + " ";
+  })
+  return capEntry;
+}
+
+//state initials if state is longer than 1 word
+function initLetters(state){
+  let stateEntry = "";
+  const stateArray = state.split(" ");
+  if (stateArray.length > 1){
+    $.each(stateArray, function(i, word){
+      stateEntry += word.charAt(0).toUpperCase()
+    })
+  } else{
+    stateEntry = state.charAt(0).toUpperCase() + state.slice(1);
+  }
+  return stateEntry;
+}
+
+//phone no. format
+
+function pFormat(phone){
+  let formP = "";
+  let finalP = "";
+  const pArray = phone.split("");
+  $.each(pArray, function(i, char){
+    if (!isNaN(char)){
+      formP += char;
+    }
+  })
+  finalP = `(${formP.slice(0,3)}) ${formP.slice(3, 6)}-${formP.slice(6)}`;
+  return finalP;
+}
+
+//Clickable buttons
+
+//hide and clear modal
+
+$close.click(function(e){
+  e.preventDefault();
+  $image.removeAttr('src');
+  $basicInfo.empty();
+  $secondaryInfo.empty();
+  $overlay.hide();
+})
+
+//next employee
+
+$next.click(function(e){
+  e.preventDefault();
+  
+})
+
+
+//previous employee
+
+
 //ajax request
+$(function(){
 $.ajax('https://randomuser.me/api/?results=12', {
   url: 'https://randomuser.me/api/?results=12',
   dataType: 'json',

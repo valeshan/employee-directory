db1eb4c7f791d665f0c76852819f40fd9cd3aaf5
valeshan <valeshan@gmail.com>
2017-12-21 19:00:32 +1100

    added clickModal function
    -added clickModal functionality which logs id of selected element, function is placed into the ajax request.
    -added modal element css attributes

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $("body").append($modal);
 //list of employees
 function empList(employees){
   $.each(employees, function(i, employee){
-        $empList.append('<li>'+  '<a onClick= "modalShow()" ><img src="' + employee.picture.medium +'"><div class = "info"></a><a>'
+        $empList.append('<li class= "employee" id = "'+ i + '">'+  '<a><img src="' + employee.picture.medium +'"><div class = "info">'
           + '<p class = "name"><b>' + employee.name.first.charAt(0).toUpperCase()+employee.name.first.slice(1)
           +' ' + employee.name.last.charAt(0).toUpperCase()+employee.name.last.slice(1) + '</b></p>'
           + '<p class = "email">' + employee.email + '</p>'
@@ -22,8 +22,16 @@ function empList(employees){
 //employee modal
 function modalShow(){
 
+};
+function clickModal(){
+  $("li").click(function(e){
+    e.preventDefault();
+    const id= $(this).attr('id');
+    console.log(id);
+  });
 }
 
+$(function(){
 //ajax request
 $.ajax('https://randomuser.me/api/?results=12', {
   url: 'https://randomuser.me/api/?results=12',
@@ -32,5 +40,7 @@ $.ajax('https://randomuser.me/api/?results=12', {
 //     console.log(data.results);
     let employees = data.results;
     empList(employees);
+    clickModal();
     }
   });
+});
